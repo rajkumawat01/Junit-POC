7e53908b66d1beb2ce9be6647956bf469c9e25b5
rajkumawat01 <raj.s.kumawat01@gmail.com>
2026-04-20 23:40:44 +0530

    Committing initial files of the Junit POC

---
+++ src/main/java/com/applesauce/JunitPocApplication.java
@@ -0,0 +1,13 @@
+package com.applesauce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applesauce/JunitPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applesauce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
